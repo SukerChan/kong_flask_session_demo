b801007e140c891ecb39a8d14969011991057198
陳宇龙 <chenyulong@cuhk.edu.cn>
2021-02-25 17:24:50 +0800

    更新代码

--- kong_api.py
+++ kong_api.py
@@ -4,6 +4,7 @@ from collections import namedtuple
 from urllib.parse import urljoin
 
 import requests
+import grequests
 
 
 class KongCustomer:
@@ -132,16 +133,7 @@ class KongAPI:
         from utils import add_url_params
 
         url = add_url_params(login_url, {'apikey': key})
-        # headers = {
-        #     # 'Connection': 'keep-alive',
-        #     # 'Pragma': 'no-cache',
-        #     # 'Cache-Control': 'no-cache',
-        #     # 'Upgrade-Insecure-Requests': '1',
-        #     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', \
-        #     # 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', \
-        #     # 'Accept-Language': 'zh-CN,zh;q=0.9,zh-TW;q=0.8,en;q=0.7',
-        # }
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers, timeout=5)
         response.raise_for_status()
         return response
 
